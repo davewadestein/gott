ef41db6e7c5a2a8ce80097d599fbae1c99902396
Dave Wade-Stein <dws@developintelligence.com>
2019-08-07 23:20:51 -0600

    Removed defer/panic/recover Removed tricker file stuff

--- src/read.go
+++ src/read.go
@@ -4,23 +4,25 @@ import "os"
 
 // START OMIT
 func check(e error) { // helper function for error checking
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func main() {
-    buf := make([]byte, 1024) // create a buffer  // HL
-    f, e := os.Open("/etc/passwd") // HL
-    check(e)
-    defer f.Close() // idiomatic to defer a Close just after Open // HL
-    for {
-        n, e := f.Read(buf) // HL
-        if n == 0 { // 0 means we've hit EOF
-            break
-        }
-        check(e)
-        os.Stdout.Write(buf[:n]) // HL
-    }
+	buf := make([]byte, 1024)      // create a buffer  // HL
+	f, e := os.Open("/etc/passwd") // HL
+	check(e)
+
+	for {
+		n, e := f.Read(buf) // HL
+		if n == 0 {         // 0 means we've hit EOF
+			break
+		}
+		check(e)
+		os.Stdout.Write(buf[:n]) // HL
+	}
+	f.Close()
 }
+
 // END OMIT
